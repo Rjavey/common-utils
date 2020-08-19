08a85a2dfc553286639984f4a937a78d7428c8a7
rjavey <Rjavey@users.noreply.github.com>
2020-08-19 09:33:05 +0800

    Update BeanValidator.java

--- src/other/BeanValidator.java
+++ src/other/BeanValidator.java
@@ -37,4 +37,31 @@ public class BeanValidator {
         } while (errors.isEmpty());
         return errors;
     }
+    
+    /**
+     * 对象类型的验证
+     * @param first
+     * @param objects
+     * @return
+     */
+    public static Map<String, String> validateObject(Object first, Object... objects) {
+        if (objects == null && objects.length > 1) {
+            return validateList(Lists.asList(first, objects));
+        } else {
+            return validate(first, new Class[0]);
+        }
+    }
+
+    /**
+     * 检查参数是否符合类型
+     * @param param
+     * @throws RuntimeException
+     */
+    public static void check(Object param) throws RuntimeException {
+        Map<String, String> map = BeanValidator.validateObject(param);
+        if (MapUtils.isNotEmpty(map)) {
+            throw new RuntimeException(map.toString());
+        }
+    }
+    
 }
